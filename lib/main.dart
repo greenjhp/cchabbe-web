6d74524c9f328536b794927827763a5d7e47ba5e
DGSR <dgsr_smith@DGSRui-MacBookAir.local>
2022-04-16 19:29:06 +0900

    테스트

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+//test
+
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
